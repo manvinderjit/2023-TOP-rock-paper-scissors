c84f0125869e4fe7497c096a9a3f27561af83b8f
Manvinderjit <91133409+manvinderjit@users.noreply.github.com>
2023-08-04 20:22:21 -0400

    feat: Add basic structure
    Add the basic code structure for the index and javascript files

---
+++ script.js
@@ -0,0 +1,15 @@
+const selectionOptions = ["rock", "paper", "scissors"];
+
+function getComputerChoice() {
+    return selectionOptions[Math.floor(Math.random()*selectionOptions.length)];    
+}
+
+
+
+function playRound(playerSelection, computerSelection) {
+
+}
+
+function game() {
+    
+}
