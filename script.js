630aa8333b7572c0bf24682b714bc8f299da4a8d
Manvinderjit <91133409+manvinderjit@users.noreply.github.com>
2023-08-04 20:58:42 -0400

    feat: Add the play round
    Add the play round functionality that determines the winner of each round

--- script.js
+++ script.js
@@ -4,12 +4,51 @@ function getComputerChoice() {
     return selectionOptions[Math.floor(Math.random()*selectionOptions.length)];    
 }
 
-
-
 function playRound(playerSelection, computerSelection) {
 
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+  
+    let playRoundResult;
+
+    switch(true) {
+        
+        case (playerSelection == "rock" && computerSelection == "paper"):
+            playRoundResult =  ("You Lose! Paper beats Rock!");
+            break;
+
+        case (playerSelection == "rock" && computerSelection == "scissors"):
+            playRoundResult =  ("You Win! Rock beats Scissors!");
+            break;
+
+        case (playerSelection == "scissors" && computerSelection == "paper"):
+            playRoundResult =  ("You Win! Scissors beat Paper!");
+            break;
+
+        case (playerSelection == "scissors" && computerSelection == "rock"):
+            playRoundResult =  ("You Lose! Rock beats Scissors!");
+            break;
+        
+        case (playerSelection == "paper" && computerSelection == "rock"):
+            playRoundResult = ("You Win! Paper beats Rock!");
+            break;
+
+        case (playerSelection == "paper" && computerSelection == "scissors"):
+            playRoundResult = ("You Lose! Scissors beats Paper!");
+            break;
+
+        default:
+            playRoundResult = ("It's a tie!");
+    }
+    
+    return playRoundResult;
+    
 }
 
 function game() {
-    
+
+    console.log(playRound("scissors", "rock"));
+
 }
+
+game();
